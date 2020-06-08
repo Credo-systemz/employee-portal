8f86fdc7e5c7672232c93fd417577506eb2ad4ce
knmmithunmani <mithunmani2597@gmail.com>
2020-06-08 20:35:37 +0530

    Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginPageComponent
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
