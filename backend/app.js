63c28bae8c3f02747eeddb681af3dba43d3c349a
Archana <archanaeie06@gmail.com>
2020-06-09 12:17:59 +0530

    Added backend

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const bodyParser = require('body-parser')
+
+const jwt = require('jsonwebtoken');
+
+const mongodb= require('mongodb').MongoClient;
+
+const app= express();
+
+app.use(cors());
+
+ app.use(bodyParser.json());
+ 
+var db;
+
+
+mongodb.connect("mongodb+srv://EmployeePortal:Emp123@cluster0-kyu6f.mongodb.net/EmployeePortal?retryWrites=true&w=majority",(error,database)=>{
+   if(error){
+       console.log("Database access denied");
+   }
+
+   else{
+   db = database.db("EmployeePortal");
+    console.log("DB connnected");
+}
+});
+
+app.post("/register",(req,res)=>{
+
+    console.log("hi hello");
+
+    req.body._id = new Date().getTime();
+
+    db.collection("userdata").insert(req.body, (error, data)=>{
+
+        if(error)
+        {
+            res.status(401).json("You have error in insert query");
+        }
+        else {
+            res.json("User Registered Successfully");
+            console.log(data);
+        }
+    })
+
+});
+
+
---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+
+const app = require("./backend/app");
+
+const server = http.createServer(app);
+
+//  const server = http.createServer((req,res)=>{
+//     res.end("Welcome to nodeJs");
+// });
+
+
+server.listen(3000,()=>{
+    console.log("Server started on port number 3000");
+});
