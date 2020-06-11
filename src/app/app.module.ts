f57add21eabf35f01e5bfca77169ca486b1b19b4
thangarj4747 <thangaraj.sps.@gmail.com>
2020-06-11 21:08:55 +0530

    login template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -7,16 +8,20 @@ import { HttpClientModule} from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
-
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    UserLoginPageComponent
+    UserLoginPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    NgbModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
