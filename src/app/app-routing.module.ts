ffc4bb3647af2a96874a308fb03070c85691e7bb
raja-k003 <59433464+raja-k003@users.noreply.github.com>
2020-06-11 22:49:14 +0530

    done routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  
-];
+  {path:'',component:LoginComponent},
+  {path:'SignUp',component:UserLoginPageComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
