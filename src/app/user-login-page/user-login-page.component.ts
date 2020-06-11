4259cd5eee3955ef7fc8e2ecf074c5931dcf4867
raja-k003 <59433464+raja-k003@users.noreply.github.com>
2020-06-11 16:02:06 +0530

    Form Validation

--- src/app/user-login-page/user-login-page.component.ts
+++ src/app/user-login-page/user-login-page.component.ts
@@ -14,19 +14,37 @@ export class UserLoginPageComponent implements OnInit {
 
   
   ngOnInit(): void {
+    let PasswordPattern='^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\])$'
+    let EmailPattern='^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$';
     this.signUpUser = new FormGroup({
       'FirstName' : new FormControl(null,Validators.required),
       'LastName': new FormControl(null),
-      'Password':new FormControl(null,[Validators.required,Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")]),
+      'Password':new FormControl(null,[Validators.required,Validators.minLength(8)]),
       'ConfirmfPassword':new FormControl('',Validators.required),
-      'EmailId': new FormControl(null,[Validators.required,Validators.email]),
-      'Company': new FormControl(null,Validators.maxLength(30)),
-     'PhoneNum' :new FormControl(null,[Validators.maxLength(10),Validators.minLength(10)])
+      'EmailId': new FormControl(null,[Validators.required,Validators.pattern(EmailPattern)]),
+      'Company': new FormControl(null,[Validators.minLength(5), Validators.maxLength(30)]),
+     'MobileNo' :new FormControl(null,[Validators.minLength(10),Validators.maxLength(11)])
    });
    
    }
-   
-
+get FnameCtrl(){
+  return this.signUpUser.get('FirstName')
+}  
+get LnameCtrl(){
+  return this.signUpUser.get('LastName')
+}
+get PasswordCtrl(){
+  return this.signUpUser.get('Password')
+}
+get emailCtrl(){
+  return this.signUpUser.get('EmailId')
+}
+get CompanyCtrl(){
+  return this.signUpUser.get('Company')
+}
+get MobileNoCtrl(){
+  return this.signUpUser.get('MobileNo')
+}
    SignUp(){
      console.log(this.signUpUser.value);
     this.UserService.UserRegistraion(this.signUpUser.value).subscribe((data:any)=>{
