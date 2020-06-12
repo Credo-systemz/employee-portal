46ad95e012a86db378a10d4ab59cb20580073f90
raja-k003 <59433464+raja-k003@users.noreply.github.com>
2020-06-12 22:45:14 +0530

    styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,15 +17,20 @@ export class LoginComponent implements OnInit {
   constructor(public loginUser :UserService) { }
 
     ngOnInit() {
-
+      let EmailPattern='^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$';
       this.loginForm=new FormGroup({
 
-        'EmailId':new FormControl(null,Validators.required),
-        'Password': new FormControl(null,Validators.required)
+        'EmailId':new FormControl(null,[Validators.required,Validators.pattern(EmailPattern)]),
+        'Password': new FormControl(null,[Validators.required, Validators.minLength(8)])
       });  
   
   }
-
+    get  EmailIdCtrl(){
+      return this.loginForm.get('EmailId')
+    }
+    get  PasswordCtrl(){
+      return this.loginForm.get('Password')
+    }
   doLogin(){
 
   this.loginUser.userLogin(this.loginForm.value).subscribe((data:string)=>{
