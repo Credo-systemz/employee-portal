481e56e1689b344b06554b252c9e346b15a1b2ed
raja-k003 <59433464+raja-k003@users.noreply.github.com>
2020-06-25 10:28:52 +0530

    test

--- src/app/components/userprofile/userprofile.component.ts
+++ src/app/components/userprofile/userprofile.component.ts
@@ -9,16 +9,35 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class UserprofileComponent implements OnInit {
 
-  UserProfile:FormGroup;
-  country;
+ 
    countries=["India","USA","Europe","Singapore"]
+
+   UserProfile:FormGroup;
   
   constructor() { }
 
   ngOnInit(): void {
-    this.UserProfile= new FormGroup({
-      'Country':new FormControl(null, [Validators.required])
-    });
-    
-  }  
+    let EmailPattern='^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$';
+    this.UserProfile=new FormGroup ({
+      'FName':new FormControl(null, Validators.required),
+      'Email':new FormControl(null,[Validators.required,Validators.pattern(EmailPattern)]),
+      'Password':new FormControl(null,[Validators.required,Validators.minLength(8)]),
+      'Mobile':new FormControl(null,[Validators.required,Validators.pattern('/{0-9}[0-9]/')]),
+      'UidType':new FormControl(null, Validators.required),
+      'Uid':new FormControl(null, Validators.required),
+    })
+  }
+get fnamectrl(){
+  return this.UserProfile.get('Fname')
+} get Emailctrl(){
+  return this.UserProfile.get('Email')
+}  get Passwordctrl(){
+  return this.UserProfile.get('Password')
+}  get Mobilectrl(){
+  return this.UserProfile.get('Mobile')
+}
+Uprofile(){
+  
+}
+
 }
