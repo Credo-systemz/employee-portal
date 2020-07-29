a846e6b476561879f466aef77b95c70557cd345a
thangaraj4747 <thangaraj.sps@gmail.com>
2020-07-29 10:37:25 +0530

    mobile pattern fixed

--- src/app/user-login-page/user-login-page.component.ts
+++ src/app/user-login-page/user-login-page.component.ts
@@ -26,7 +26,7 @@ export class UserLoginPageComponent implements OnInit {
    
     let PasswordPattern='^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\])$'
     let EmailPattern='^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,32})$';
-    let mobNumberPattern = "^((\\+91-?)|0)?[0-9]{10}$";  
+    let mobNumberPattern = "^([9876]{1})([0-9]{9})$";  
 
     
 
